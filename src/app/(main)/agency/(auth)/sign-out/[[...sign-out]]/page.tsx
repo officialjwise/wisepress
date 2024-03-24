32e5009f5448e55b607c070c58f79c97768c0f5b
officialjwise <officialjwise20@gmail.com>
2024-03-24 10:04:49 +0000

    - Worked on the hompage, routes and the middleware

---
+++ src/app/(main)/agency/(auth)/sign-out/[[...sign-out]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignIn, SignUp, SignedOut } from '@clerk/nextjs'
+import React from 'react'
+
+const Page = () => {
+  return <SignUp />
+}
+
+export default Page
---
+++ src/app/site/layout.tsx
@@ -0,0 +1,18 @@
+import Navigation from '@/components/site/navigation'
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+import { dark } from '@clerk/themes'
+
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>{children}
+      <main className="h-full">
+        <Navigation />
+        {children}
+      </main>        
+    </ClerkProvider>
+  )
+}
+
+export default layout
